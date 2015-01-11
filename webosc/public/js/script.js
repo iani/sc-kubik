ce82341e2365075c066ecdf3e29fc7ce181186bc
Pral2a <guillem@pral2a.com>
2015-01-11 16:25:25 +0100

    improved ui for tests

--- webosc/public/js/script.js
+++ webosc/public/js/script.js
@@ -1,27 +1,52 @@
 var socket = io();
 
+
 jQuery(document).ready(function ($) {
+
+    oscSlider("browser", 440, 4400);
 
     socket.on('users', function (msg) {
         console.log("Clients: " + msg)
     });
 
     socket.on('osc', function (msg) {
-        console.log(msg)
+        console.log(msg)  
     });
-
-    $(".sound").mouseup(function () {
-        var value = new Number($(this).val());
-        var msg = {
-            address: "/browser", 
-            args: [
-                value
-            ]
-        };
-        socket.emit('osc', msg);    
-    });
-
 
 });
 
 
+function oscSlider(oscPath, min, max) {
+
+    $('<label>')
+        .attr('for',oscPath)
+        .text(oscPath)
+        .appendTo('.controllers');
+
+    $('<input>')
+        .attr('class',oscPath)
+        .attr('id',oscPath)
+        .attr('type','range')
+        .attr('min',min)
+        .attr('max',max)
+        .appendTo('.controllers');
+
+
+    $('.' + oscPath).rangeslider({
+        polyfill: false,
+        onSlide: function(position, value) {
+            var value = new Number(value);
+            var msg = {
+                address: '/' + oscPath, 
+                args: [
+                    value
+                ]
+            };
+            socket.emit('osc', msg);
+
+            $('label[for=' + oscPath + ']').text(oscPath + ' ' + value);
+        }
+    });
+
+    //missing bidirectional control
+}
